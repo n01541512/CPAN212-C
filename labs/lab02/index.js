ae0f00507bde4ac68f7eb8b498afba2026b8c482
n01541512 <n01541512@humber.ca>
2025-01-23 20:24:04 -0500

    more changes

--- labs/lab02/index.js
+++ labs/lab02/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import labRouter from './router/lab_router.js';
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 app.use("/lab", labRouter);
 
