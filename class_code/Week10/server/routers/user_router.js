d70870b19f20ec8a22711612742579351964acc6
n01541512 <n01541512@humber.ca>
2025-03-20 18:30:24 -0400

    updated

--- class_code/Week10/server/routers/user_router.js
+++ class_code/Week10/server/routers/user_router.js
@@ -8,14 +8,14 @@ router.post("/register", (req, res) => {
     const { email, password } = req.body;
     bcrypt.hash(password, 10)
         .then((hashedPassword) => {
-            let newUser = new User( {
+            let newUser = new User({
                 email,
                 password: hashedPassword
             });
 
             newUser.save()
                 .then(() => {
-                    res.json({message: "Account Registered"})
+                    res.json({ message: "Account Registered" })
                 })
                 .catch((err) => {
                     console.log(err);
@@ -27,5 +27,29 @@ router.post("/register", (req, res) => {
             return res.json({ message: "Could not complete transaction" })
         })
 });
+
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email: email })
+        .then((user_account) => {
+            if (!user_account) {
+                return res.json({ message: "User account not found" })
+            }
+
+            bcrypt
+                .compare(password, user_account.password)
+                .then()
+                .catch((err) => {
+                    console.log(err);
+                    return res.json({ message: "Could not complete request" })
+                })
+
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: "Could not complete request" })
+        })
+})
 
 export default router;
